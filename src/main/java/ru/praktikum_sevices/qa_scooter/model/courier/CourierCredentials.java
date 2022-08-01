23369eb1bb1f3f4be7ae0743d584b81afb1fc4a5
Viacheslav G <obstacle400@gmail.com>
2022-08-01 14:57:52 +0700

    first commit

---
+++ src/main/java/ru/praktikum_sevices/qa_scooter/model/courier/CourierCredentials.java
@@ -0,0 +1,41 @@
+package ru.praktikum_sevices.qa_scooter.model.courier;
+
+public class CourierCredentials {
+
+    private String login;
+
+    private String password;
+
+    public CourierCredentials(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+
+    public CourierCredentials() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "CourierCredentials{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
